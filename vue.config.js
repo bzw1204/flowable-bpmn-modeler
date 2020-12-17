20bd3af086857ea2808477e472612195dadcfcec
白振伟 <baizw@cnns.net>
2020-12-17 17:16:04 +0800

    1. 编译demo

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ let publicPath = '/'
 let outputDir = 'dist'
 let appFileName = 'Demo'
 if (NODE_ENV === 'demo') {
-  publicPath = '/workflow-bpmn-modeler/demo/'
+  publicPath = '/flowable-bpmn-modeler/demo/'
   outputDir = 'docs/demo'
   appFileName = 'Demo'
 }
@@ -16,7 +16,7 @@ if (NODE_ENV === 'cdn') {
   if (cdnVersionList.includes(version)) {
     throw new Error(`cdn 版本 ${version} 已存在`)
   }
-  publicPath = `https://cdn.jsdelivr.net/gh/goldsubmarine/workflow-bpmn-modeler@${version}/docs/cdn/${version}/`
+  publicPath = `https://cdn.jsdelivr.net/gh/goldsubmarine/flowable-bpmn-modeler@${version}/docs/cdn/${version}/`
   outputDir = `docs/cdn/${version}`
   appFileName = 'Lib'
 }
@@ -66,37 +66,33 @@ module.exports = {
   // 如果你不需要使用eslint，把lintOnSave设为false即可
   lintOnSave: true,
   chainWebpack: config => {
-    config.resolve.alias
-      .set('views', resolve('src/views'))
-
-    config
-      .when(NODE_ENV !== 'development' && NODE_ENV !== 'build',
-        config => {
-          config
-            .optimization.splitChunks({
-              chunks: 'all',
-              cacheGroups: {
-                libs: {
-                  name: 'chunk-libs',
-                  test: /[\\/]node_modules[\\/]/,
-                  priority: 10,
-                  chunks: 'initial' // only package third parties that are initially dependent
-                },
-                elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                },
-                vue: {
-                  name: 'chunk-vue', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?vue(.*)/ // in order to adapt to cnpm
-                }
-              }
-            })
-          config.optimization.runtimeChunk('single')
-        }
-      )
+    config.resolve.alias.set('views', resolve('src/views'))
+    config.when(NODE_ENV !== 'development' && NODE_ENV !== 'build',
+      config => {
+        config.optimization.splitChunks({
+          chunks: 'all',
+          cacheGroups: {
+            libs: {
+              name: 'chunk-libs',
+              test: /[\\/]node_modules[\\/]/,
+              priority: 10,
+              chunks: 'initial' // only package third parties that are initially dependent
+            },
+            elementUI: {
+              name: 'chunk-ant-design-vue', // split elementUI into a single package
+              priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+              test: /[\\/]node_modules[\\/]_?ant-design-vue(.*)/ // in order to adapt to cnpm
+            },
+            vue: {
+              name: 'chunk-vue', // split elementUI into a single package
+              priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+              test: /[\\/]node_modules[\\/]_?vue(.*)/ // in order to adapt to cnpm
+            }
+          }
+        })
+        config.optimization.runtimeChunk('single')
+      }
+    )
   },
   // 设为false打包时不生成.map文件
   productionSourceMap: false,
